88e41432fc4d8478b98c558695cc410fc961b07a
Jeremiah Hetzel <117078336+melonmusk2@users.noreply.github.com>
2023-12-24 12:59:43 +0100

    Update dino.js

--- 24_game/dino.js
+++ 24_game/dino.js
@@ -20,9 +20,11 @@ export function setupDino() {
   currentFrameTime = 0
   yVelocity = 0
   setCustomProperty(dinoElem, "--bottom", 0)
-  document.removeEventListener("keydown", onJump)
-  document.addEventListener("keydown", onJump)
-  document.addEventListener("touchstart", onJump)
+  document.removeEventListener("keydown", onJump);
+  document.removeEventListener("touchstart", onJump);
+
+  document.addEventListener("keydown", onJump);
+  document.addEventListener("touchstart", onJump);
 }
 
 export function updateDino(delta, speedScale) {
